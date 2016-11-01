d4d32386bfad5a2290f043fda5ea51d77bb6d14d
IanMeyers <meyersi@amazon.com>
2016-11-01 09:13:08 +0000

    Fixed sys.argv check

--- src/deprovision_tables.py
+++ src/deprovision_tables.py
@@ -5,4 +5,10 @@ import setup_existing_tables
 
 
 if __name__ == "__main__":
-    setup_existing_tables.deprovision(sys.argv[1])
+    if len(sys.argv) != 2:
+        print "Usage: deprovision_tables.py whitelist_configuration.hjson"
+        sys.exit(-1)
+    else:
+        setup_existing_tables.deprovision(sys.argv[1])
+        
+    
