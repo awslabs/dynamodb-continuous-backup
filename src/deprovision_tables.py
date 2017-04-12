3c65ca0563345c43695d8d305594b9733769d246
Alex Gray <agray@clearcareonline.com>
2017-04-11 22:01:31 -0400

    Minor fixes and enhancements. No functional changes.

--- src/deprovision_tables.py
+++ src/deprovision_tables.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+
+# add the lib directory to the path
+sys.path.append('lib')
+
 import setup_existing_tables
+import argparse
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage: deprovision_tables.py whitelist_configuration.hjson"
-        sys.exit(-1)
-    else:
-        setup_existing_tables.deprovision(sys.argv[1])
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('whitelist_configuration', help='whitelist_configuration.hjson')
+    args = parser.parse_args()
+
+    setup_existing_tables.deprovision(args.whitelist_configuration)
